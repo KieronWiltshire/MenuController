b4b979572e221c526d0ad0445b5c01f2a4e661f5
Kieron Wiltshire <kieron.wiltshire@outlook.com>
2015-04-18 23:00:37 +0100

    added some methods to the InventoryMenu

--- src/io/teamelite/brush_menu/system/InventoryMenu.java
+++ src/io/teamelite/brush_menu/system/InventoryMenu.java
@@ -5,6 +5,9 @@ import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @name 		BrushMenu
  * @author 		Liam Reffell and Kieron Wiltshire
@@ -19,6 +22,7 @@ public class InventoryMenu {
 
     // Instance properties
     private Inventory inventory;
+    private HashMap<Integer, MenuItem> items;
 
     /**
      * MenuInventory constructor
@@ -27,6 +31,7 @@ public class InventoryMenu {
      */
     public InventoryMenu(InventoryHolder holder, String title) {
         this.inventory = Bukkit.createInventory(holder, InventoryType.CHEST, title);
+        this.items = new HashMap<Integer, MenuItem>();
     }
 
     /**
@@ -36,6 +41,59 @@ public class InventoryMenu {
      */
     public Inventory getInventory() {
         return this.inventory;
+    }
+
+    /**
+     * Add a MenuItem to the inventory
+     *
+     * @param slot The inventory slot of the item
+     * @param item The item to add to the inventory menu
+     */
+    public void addItem(int slot, MenuItem item) {
+        this.items.put(slot, item);
+    }
+
+    /**
+     * Add a MenuItem to the inventory
+     *
+     * @param item The item to add to the inventory menu
+     */
+    public void addItem(MenuItem item) {
+        for(int i = 0; i < inventory.getSize(); i++) {
+            if (!this.items.containsKey(i)) {
+                this.addItem(i, item);
+                return;
+            }
+        }
+    }
+
+    /**
+     * Remove an item from the inventory
+     *
+     * @param slot The item slot of which to remove
+     */
+    public void removeItem(int slot) {
+        if (this.items.containsKey(slot)) {
+            this.items.remove(slot);
+
+            // Remove it from the Inventory instance itself
+            // TODO
+        }
+    }
+
+    /**
+     * Remove an item from the inventory
+     *
+     * @param item The item to remove
+     */
+    public void removeItem(MenuItem item) {
+        if (this.items.containsValue(item)) {
+            for (Map.Entry<Integer, MenuItem> entry : this.items.entrySet()) {
+                if (entry.getValue().equals(item)) {
+                    this.removeItem(entry.getKey());
+                }
+            }
+        }
     }
 
 }
