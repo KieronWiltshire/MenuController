bf139f327ca10aeecc755ae7a39d657147502726
Kieron Wiltshire <kieron.wiltshire@outlook.com>
2015-04-18 23:45:13 +0100

    comment cleanup

--- src/io/teamelite/brush_menu/system/json/adapters/ItemStackAdapter.java
+++ src/io/teamelite/brush_menu/system/json/adapters/ItemStackAdapter.java
@@ -1,13 +1,14 @@
 package io.teamelite.brush_menu.system.json.adapters;
 
 /**
- * @author Liam Reffell and Kieron Wiltshire
- * @name BrushMenu
- * @contact http://www.mcteamelite.com/
- * @license MIT License
- * @description The plugin requires Java 1.6 or higher and depends on the VoxelSniper
- * Bukkit plugin. It allows a user to open up an inventory interface and
- * select their desired VoxelSniper brush.
+ * @name 		BrushMenu
+ * @author 		Liam Reffell and Kieron Wiltshire
+ * @contact 	http://www.mcteamelite.com/
+ * @license 	MIT License
+ * @description
+ * 				The plugin requires Java 1.6 or higher and depends on the VoxelSniper
+ * 				Bukkit plugin. It allows a user to open up an inventory interface and
+ * 				select their desired VoxelSniper brush.
  */
 public class ItemStackAdapter {
 }
