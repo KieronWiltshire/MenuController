bac68032e4dda3be3435aa7f2087eaada5bddb11
Kieron Wiltshire <kieron.wiltshire@outlook.com>
2015-04-18 23:12:20 +0100

    added methods to the MenuItem class

--- src/io/teamelite/brush_menu/system/MenuItem.java
+++ src/io/teamelite/brush_menu/system/MenuItem.java
@@ -1,5 +1,7 @@
 package io.teamelite.brush_menu.system;
 
+import org.bukkit.inventory.ItemStack;
+
 /**
  * @name 		BrushMenu
  * @author 		Liam Reffell and Kieron Wiltshire
@@ -10,5 +12,41 @@ package io.teamelite.brush_menu.system;
  * 				Bukkit plugin. It allows a user to open up an inventory interface and
  * 				select their desired VoxelSniper brush.
  */
-public class MenuItem {
+public abstract class MenuItem {
+
+    // Instance properties
+    private ItemStack item;
+
+    /**
+     * MenuItem constructor
+     *
+     * @param item The item to represent the menu option
+     */
+    protected MenuItem(ItemStack item) {
+        this.item = item;
+    }
+
+    /**
+     * Set the item
+     *
+     * @param item The item to represent the menu option
+     */
+    public void setItem(ItemStack item) {
+        this.item = item;
+    }
+
+    /**
+     * Get the item
+     *
+     * @return The item which represents the menu option
+     */
+    public ItemStack getItem() {
+        return this.item;
+    }
+
+    /**
+     * This method should be called if the item was selected
+     */
+    public abstract void onItemSelect();
+
 }
