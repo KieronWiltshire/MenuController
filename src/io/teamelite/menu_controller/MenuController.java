99cac8988dd676619ec356382d1901d392092e47
Kieron Wiltshire <kieron.wiltshire@outlook.com>
2015-04-19 00:44:50 +0100

    Decided to go the API route and add a proxy class

---
+++ src/io/teamelite/menu_controller/MenuController.java
@@ -0,0 +1,26 @@
+package io.teamelite.menu_controller;
+
+
+/**
+ * @name 		MenuController
+ * @author 		Liam Reffell and Kieron Wiltshire
+ * @contact 	http://www.mcteamelite.com/
+ * @license 	MIT License
+ * @description
+ * 				The plugin requires Java 1.6 or higher.
+ * 				It allows a user to open up an inventory interface and
+ * 				select their saved menu options.
+ */
+public class MenuController {
+
+    /**
+     * MenuController constructor
+     */
+    private MenuController() { /* private constructor to prevent developer access */ }
+
+    /**
+     * Plugin proxy class
+     */
+    priate
+
+}
